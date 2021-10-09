249c7db2e56ca0052490c3dd02fba55f76dc9995
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-09 06:50:20 +0530

    [Rupali]feat:Added Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day7_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day 7 Assignments");
+        }
+    }
+}
