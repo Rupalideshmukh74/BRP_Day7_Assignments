34ebb28bfc9c4e73347c7bf97457848857cfb067
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-10-13 21:58:23 +0530

    [Rupali]feat:Prime Factorization program

---
+++ PrimeFactor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day7_Assignments
+{
+    public class PrimeFactor
+    {
+       
+    }
+}
+       
+
+    
+
--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace Day7_Assignments
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Day 7 Assignments");
+            FindPrimeFactors();
+
+
+        }
+            public static void FindPrimeFactors()
+            {
+                Console.WriteLine("Finding prime factors\n");
+                int num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Prime factors of " + num);
+                for (int i = 2; i * i <= num; i++)
+                {
+                    while (num % i == 0)
+                    {
+                        num /= i;
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (num >= 2)
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+
+
+
         }
     }
-}
